391892a7a4c43a758006b410960b999d12b70fe2
Vítor Moreira Rodrigues <vitorrdrgs04@gmail.com>
2024-08-19 11:27:44 -0300

    changed some keymaps

--- lua/plugins/keymaps.lua
+++ lua/plugins/keymaps.lua
@@ -1,9 +1,9 @@
 -- Leader key
 vim.g.mapleader = ' '
 
--- FZF
-vim.keymap.set('n', '<C-p>', ':Files<CR>')  -- File search
-vim.keymap.set('n', '<leader>ff', ':Ag<CR>')  -- Text search
+-- FZF and Telescope
+vim.keymap.set('n', '<leader>fs', ':Telescope find_files<CR>')  -- File search
+vim.keymap.set('n', '<leader>ff', ':Telescope live_grep<CR>')  -- Text search
 
 -- Nvim Tree
 vim.keymap.set('n', '<leader>e', ':NvimTreeFindFileToggle<CR>') -- Open Nvim Tree Find File
