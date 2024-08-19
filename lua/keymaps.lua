67d3c8279d3ee4f8c7c663ed560cfba70aded4aa
Vítor Moreira Rodrigues <vitorrdrgs04@gmail.com>
2024-08-18 23:13:24 -0300

    changed buffers keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -9,17 +9,15 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set('n', '<leader>wr', ':w<CR>') -- Write changes
 vim.keymap.set('n', '<leader>qu', ':q<CR>') -- Quit nvim
-vim.keymap.set('n', '<leader>bn', ':bn<CR>') -- Buffer next
-vim.keymap.set('n', '<leader>bp', ':bp<CR>') -- Buffer previous
-vim.keymap.set('n', '<leader>bd', ':bd<CR>') -- Buffer delete
+vim.keymap.set('n', '<leader>n', ':bn<CR>') -- Buffer next
+vim.keymap.set('n', '<leader>p', ':bp<CR>') -- Buffer previous
+vim.keymap.set('n', '<leader>x', ':bd<CR>') -- Buffer delete
 
 vim.keymap.set('n', ',v', '<C-w>v') -- Vertical split
 vim.keymap.set('n', ',w', '<C-w>s') -- Horizonta split
 vim.keymap.set('n', ',c', '<C-w>c') -- Close split
 
 vim.keymap.set('n', '<Esc>', ':nohlsearch<CR>') -- Remove highlight search
-
-vim.keymap.set('n', '<leader>pv', ':Ex<CR>') -- Open nvim explorer
 
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>') -- Move focus to the left window
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>') -- Move focus to the right window
