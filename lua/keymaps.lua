a755184941a059bfebe59dbc641c3794ae264a91
Vítor Moreira Rodrigues <vitorrdrgs04@gmail.com>
2024-09-06 09:30:01 -0300

    removed deprecated language servers from mason

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -13,7 +13,7 @@ vim.keymap.set('n', '<leader>n', ':bn<CR>') -- Buffer next
 vim.keymap.set('n', '<leader>p', ':bp<CR>') -- Buffer previous
 vim.keymap.set('n', '<leader>x', ':bd<CR>') -- Buffer delete
 
-vim.keymap.set("n", "<C-b>", "<C-u>zz") -- Scroll up and center
+vim.keymap.set("n", "<C-u>", "<C-u>zz") -- Scroll up and center
 vim.keymap.set("n", "<C-d>", "<C-d>zz") -- Scroll down and center
 
 vim.keymap.set('n', '<C-c>', ':nohlsearch<CR>') -- Remove highlight search
