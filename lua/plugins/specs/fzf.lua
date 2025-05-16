bb6fccd5fd1ab4713097696628b35309c7242454
Vítor Moreira Rodrigues <vitorrdrgs04@gmail.com>
2025-05-16 14:49:24 -0300

    Fixed fzf error

--- lua/plugins/specs/fzf.lua
+++ lua/plugins/specs/fzf.lua
@@ -1,8 +1,3 @@
 return {
-    'junegunn/fzf',
-<<<<<<< HEAD
-    -- dir = '~/.fzf',
-    -- build = './install --all'
-=======
->>>>>>> 71f528d (Changed some configs and deprecated functions)
-  }
+	"junegunn/fzf",
+}
